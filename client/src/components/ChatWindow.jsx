52bf85c1efc051394f0950d180ddde3bb00ab965
VIJAY VARMA V <vvarmavijay18@gmail.com>
2026-04-18 16:30:30 +0530

    UI Fix: Added dedicated scroll spacer to prevent message bar overlap

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -17,7 +17,7 @@ export default function ChatWindow({ messages, onSpeak, onStopSpeak, speakingTex
   ];
 
   return (
-    <div className="flex-1 overflow-y-auto px-4 md:px-12 pt-8 pb-48 scrollbar-hide z-10 w-full relative">
+    <div className="flex-1 overflow-y-auto px-4 md:px-12 pt-8 pb-8 scrollbar-hide z-10 w-full relative">
       <div className="max-w-4xl mx-auto flex flex-col h-full">
         {messages.length === 0 ? (
           <motion.div 
@@ -81,6 +81,8 @@ export default function ChatWindow({ messages, onSpeak, onStopSpeak, speakingTex
                 isSpeakingThis={speakingText === message.content}
               />
             ))}
+            {/* Spacer to push content above InputBar */}
+            <div className="h-48 shrink-0" />
             <div ref={endRef} />
           </div>
         )}
